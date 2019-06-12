4152a02d888b0ed5baa0216b4836d19b1d3a8b38
tuntawin <tuntawin@hotmail.com>
2019-06-12 11:55:23 +0700

    update key

--- API/Controllers/LoginController.cs
+++ API/Controllers/LoginController.cs
@@ -57,8 +57,7 @@ namespace API.Controllers
                 new Claim(ClaimTypes.Name, username)
             });
 
-            const string sec = "401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
-            var now = DateTime.UtcNow;
+            const string sec = "secret key";
             var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(sec));
             var signingCredentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(securityKey, Microsoft.IdentityModel.Tokens.SecurityAlgorithms.HmacSha256Signature);
 
